8c93bb9e884fd704b695ea9ce1354c3318a95dba
RiverRay <laolei@forkway.cn>
2022-09-14 15:38:46 +0800

    chore: add algoria

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -57,6 +57,11 @@ export default {
             ]
         },
         sidebar: sidebar,
+        algolia: {
+            apiId: 'CN4JQQMG9O',
+            apiKey: 'e2b81d64b59e6b5bc9bcfcb9487d2793',
+            indexName: 'figma_sheet_sync'
+        },
         locales: {
             '/zh/': {
                 nav: navbar_cn,
