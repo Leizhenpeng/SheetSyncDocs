a5058eeaef9b09fd79a47a699e9605977e6a2e07
RiverRay <laolei@forkway.cn>
2022-11-05 21:04:12 +0800

    feat: 添加致谢

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -1,18 +1,37 @@
 import {DefaultTheme} from 'vitepress';
 
+const sec1 = {
+    text: '简单来说',
+    collapsible: true,
+    items: [
+        { text: '诞生日记', link: '/guide/whyIt.md' },
+        { text: '使用场景', link: '/guide/useCase' },
+        { text: '快速上手', link: '/guide/start' },
+        { text: '我们的不同', link: '/guide/enhance' },
+    ],
+};
+
+const sec2 :DefaultTheme.SidebarGroup= {
+    text:'复杂来讲',
+    collapsible: true,
+    items:[
+        {text:'表格内容',link:'/guide/table'},
+    ]
+}
+
+const sec3 :DefaultTheme.SidebarGroup= {
+    text:'顺便提一下',
+    items:[
+        {text:'致谢',link:'/guide/other/thanks'},
+    ]
+}
+
+
+
+
 export const sidebar: DefaultTheme.Sidebar = {
+    '/guide/': [sec1,sec2,sec3
 
-    '/guide/': [
-        {
-            text: '简单说',
-            items: [
-                { text: '诞生日记', link: '/guide/whyIt.md' },
-                { text: '使用场景', link: '/guide/useCase' },
-                { text: '快速上手', link: '/guide/start' },
-                { text: '我们的不同', link: '/guide/enhance' },
-            ],
-
-        },
     ],
     '/en/guide/': [{
         text: 'Guide',
